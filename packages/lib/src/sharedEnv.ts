967ea679e5204b079f71eb267789ac9bdac6089d
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-10-09 12:16:53 -0400

    📚 Set up shared library
    - Switched env management to dotenv
    - Added t3 env for type safety and intellisense

---
+++ packages/lib/src/sharedEnv.ts
@@ -0,0 +1,16 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+import { config } from "dotenv";
+
+config({
+  path: "../../../.env",
+});
+
+export const env = createEnv({
+  clientPrefix: "PUBLIC_",
+  client: {
+    PUBLIC_ASSETS: z.string().url(),
+  },
+  runtimeEnv: process.env,
+  emptyStringAsUndefined: true,
+});
