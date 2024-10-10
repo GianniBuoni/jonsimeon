ef9b9f53013bb1f10b9d5b29a31a474db79b6de8
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-10-10 14:09:43 -0400

    🚀 Started building the blog app
    - Switched astro configuration files to typescript
    - Modified the amount of parallel tasks in root package.json

---
+++ apps/main/astro.config.ts
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  publicDir: "../../public",
+  site: "https://jonsimeon.com",
+  integrations: [tailwind(), react()],
+  output: "hybrid",
+
+  adapter: node({
+    mode: "standalone",
+  }),
+});
