1cbacf35efb174f57765ab029e1a62d6477896f1
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-10-12 11:30:05 -0400

    Built CarouselControls component
    - Requires a store prop which decided which store it contols!

---
+++ packages/ui/src/react/carousel/CarouselControls.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+
+import useCarouselStore from "@lib/stores/useCarouselStore";
+import useModalStore from "@lib/stores/useModalStore";
+import type { CarouselSettings } from "@lib/stores/types";
+
+interface Props {
+  store: "carousel" | "modal";
+  settings: CarouselSettings;
+}
+
+const CarouselControls = ({ store, settings }: Props) => {
+  const currentStore =
+    store === "carousel" ? useCarouselStore() : useModalStore();
+  const { increment, decrement, page } = currentStore;
+
+  return (
+    <div className="w-full h-full flex justify-between">
+      <NavIcon direction="previous" onClick={() => decrement(settings)} />
+      {page.toString()}
+      <NavIcon direction="forward" onClick={() => increment(settings)} />
+    </div>
+  );
+};
+
+// Nav Icon Component defined here.
+// CarousleControls is the only component that consumes this.
+const options = {
+  forward: { label: "Next", icon: "right" },
+  previous: { label: "Previous", icon: "left" },
+};
+
+interface NavProps extends React.HTMLAttributes<HTMLElement> {
+  direction: keyof typeof options;
+}
+
+const NavIcon = ({ direction, ...rest }: NavProps) => {
+  const { label, icon } = options[direction];
+  return (
+    <div
+      className="cursor-pointer flex items-center text-accent gap-2"
+      {...rest}
+    >
+      {direction == "forward" && <span>{label}</span>}
+      <Icon
+        icon={`mdi:arrow-${icon}-circle`}
+        className="hover:scale-105 text-3xl"
+      />
+      {direction == "previous" && <span>{label}</span>}
+    </div>
+  );
+};
+
+export default CarouselControls;
