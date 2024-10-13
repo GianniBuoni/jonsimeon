a5546e902088d5863b160e454ecfadd6588d3d39
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-10-13 11:19:55 -0400

    🧹 Cleaning up unnecessary files and deps
    - dotEnv removed from project: NX is managing env
    - env file and vault moved to project root

--- packages/lib/src/sharedEnv.ts
+++ packages/lib/src/sharedEnv.ts
@@ -1,10 +1,5 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
-import { config } from "dotenv";
-
-config({
-  path: "../.env",
-});
 
 export const env = createEnv({
   server: {
