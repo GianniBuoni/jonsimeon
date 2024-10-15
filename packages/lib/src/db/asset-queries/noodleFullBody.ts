2b0fab43262f848b36b50e80c0d27a978d329fc0
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-10-14 20:27:56 -0400

    Added image of Noodle to the footer!
    TO DO: Go through and moddify breakpoints for phones & tablets

---
+++ packages/lib/src/db/asset-queries/noodleFullBody.ts
@@ -0,0 +1,12 @@
+import { readItems } from "@directus/sdk";
+import dbClient from "../directus";
+
+const request = await dbClient.request(
+  readItems("site_assets", {
+    fields: [{ image: ["id", "description"] }],
+    filter: { title: { _eq: "noodle-full-body" } },
+    limit: 1,
+  }),
+);
+
+export const noodleFullBody = request[0].image;
