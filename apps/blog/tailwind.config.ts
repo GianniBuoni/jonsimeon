80f9734c8e7d1f428423da07ed430948473ddaaf
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-10-19 14:35:38 -0400

    Prepping for a migration back to trubo
    - Was working in a test branch since I was having issues with deployment
    - Decided to go back to turbo just for the docker pruning feature
    - Figured out that the Turbo TUI was the culprit of my CPU issues

--- apps/blog/tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "tailwindcss";
-import sharedConfig from "@jonsimeon/tailwind-config";
-
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  presets: [sharedConfig],
-} satisfies Config;
--- packages/ui/tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from "tailwindcss";
-import sharedConfig from "@jonsimeon/tailwind-config";
-
-export default {
-  content: ["./src/**/*.{astro,tsx}"],
-  presets: [sharedConfig],
-} satisfies Config;
