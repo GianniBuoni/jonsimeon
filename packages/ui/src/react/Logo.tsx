97f3a10b4ae8ce36f3d0b4ae0e422ed5f3009fdc
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-11-05 14:16:29 -0500

    Changed Logo component again
    - Wrapping the div in the link again
    - This time, added a width to the link element!

--- packages/ui/src/react/Logo.tsx
+++ packages/ui/src/react/Logo.tsx
@@ -4,8 +4,8 @@ const heading = "Jon Simeon";
 
 const Logo = () => {
   return (
-    <div className="bg-accent py-1 px-3 w-fit rounded-full self-start z-20 hover:scale-110 transition-transform">
-      <a href="/" className="z-20">
+    <a href="/" className="z-20 w-fit">
+      <div className="bg-accent py-1 px-3 w-fit rounded-full self-start z-20 hover:scale-110 transition-transform">
         <Heading
           tagName="h1"
           size="h3"
@@ -14,8 +14,8 @@ const Logo = () => {
         >
           {heading.toUpperCase()}
         </Heading>
-      </a>
-    </div>
+      </div>
+    </a>
   );
 };
 
