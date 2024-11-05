27c55efc8cb812c2b5c101f521c9417ea0ef3432
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-11-05 13:23:39 -0500

    ✨ Added link styling to the AboutBody

--- apps/main/src/components/react/AboutBody.tsx
+++ apps/main/src/components/react/AboutBody.tsx
@@ -12,7 +12,9 @@ const AboutBody = ({ sections }: Props) => {
       <Heading tagName="h2" size="h1" classes="mb-3 w-[30rem] text-accent">
         {s.heading}
       </Heading>
-      <Markdown className="prose">{s.body}</Markdown>
+      <Markdown className="prose prose-a:text-accent prose-a:no-underline hover:prose-a:underline prose-a:transition-opacity prose-a:decoration-2">
+        {s.body}
+      </Markdown>
     </section>
   ));
 };
