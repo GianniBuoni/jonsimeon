269200408ee599e45b7b71839162d3051a7fa9df
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-12-18 11:23:08 -0500

    Placeholder/WIP message
    - Done so I can keep working on main/ and blog/ w/o making a new branch
    - Root docker-compose now builds and runs resume/ and placeholder/

---
+++ apps/placeholder/astro.config.ts
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
+import react from "@astrojs/react";
+
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  publicDir: "../../public",
+  site: "https://jonsimeon.com",
+  integrations: [tailwind(), react(), icon()],
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone",
+  }),
+});
